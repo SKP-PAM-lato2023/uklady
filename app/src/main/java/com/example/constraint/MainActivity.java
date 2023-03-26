f8f321f34ec7b8294ff628bfe133ec64815f7e75
Adam Karczewski <akarczewski22@gmail.com>
2023-03-26 13:10:56 +0200

    Ukady

---
+++ app/src/main/java/com/example/constraint/MainActivity.java
@@ -0,0 +1,20 @@
+package com.example.constraint;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+       // setContentView(R.layout.activity_main);
+       // setContentView(R.layout.okienko);
+       // setContentView(R.layout.tabela);
+       // setContentView(R.layout.grid);
+        //setContentView(R.layout.uklad);
+        setContentView(R.layout.scroll_view);
+
+    }
+}
